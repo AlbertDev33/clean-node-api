2fb412febfc357835e76b37a81c2cce608212bc1
AlbertDev33 <alber725@gmail.com>
2021-02-02 16:02:11 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,30 +1,38 @@
-import InvalidParamError from '../errors/invalid-param-error';
+import ServerError from '../errors/server-error';
 import { IHttpResponse, IHttpRequest } from '../protocols/http';
 import MissingParamError from '../errors/missing-param-error';
 import { badRequest } from '../helpers/http-helper';
 import { IController } from '../protocols/controller';
 import { IEmailValidator } from '../protocols/email-validator';
+import InvalidParamError from '../errors/invalid-param-error';
 
 export default class SignUpController implements IController {
   constructor(private emailValidator: IEmailValidator) {}
 
   handle(httpRequest: IHttpRequest): IHttpResponse {
-    const requiredFields = [
-      'name',
-      'email',
-      'password',
-      'passwordConfirmation',
-    ];
-    // eslint-disable-next-line no-restricted-syntax
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const requiredFields = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation',
+      ];
+      // eslint-disable-next-line no-restricted-syntax
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
 
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'));
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'));
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
     return { statusCode: 400, body: '' };
   }
