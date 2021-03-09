d751141129b354ec697568ae2aed549478280c3b
AlbertDev33 <alber725@gmail.com>
2021-03-09 08:23:12 -0300

    chore: exclude main folder coverage

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   preset: '@shelf/jest-mongodb',
   testEnvironment: 'node',
-  testMatch: ['**/*.spec.ts'],
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
